d70d685a0a8f0721d9983cf8f821097d0610d7bf
xlikc0328 <15104076298@163.com>
2020-12-08 10:03:17 +0800

    app打包配置为生产环境

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
 
 module.exports = {
+  //webpack配置
+  configureWebpack: {
+    //关闭 webpack 的性能提示
+    performance: {
+      hints: false
+    }
+  },
+
+  //警告 webpack 的性能提示
+
   // 解决跨域请求问题 CORS
   devServer: {
     // host: localhost,
@@ -9,8 +19,8 @@ module.exports = {
     // https: true,
     hotOnly: true, // 热更新
     // 需要转发到的后端服务器地址 经过此配置后只需要访问 前端地址即可 如 localhost:8080
-    proxy: 'http://localhost:8020' // 开发环境
-    // proxy: 'http://47.99.65.198:8020' // 生产环境
+    // proxy: 'http://localhost:8020' // 开发环境
+    proxy: 'http://47.99.65.198:8020' // 生产环境
   },
 
   publicPath: process.env.NODE_ENV === 'production' ? './' : './',
